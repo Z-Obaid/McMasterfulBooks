5ecbc6b35deb48b37842bcfa5924046f3483bb13
Z-Obaid <82549386+Z-Obaid@users.noreply.github.com>
2026-01-31 19:09:05 +0000

    import incorrect

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 // eslint.config.mjs
 import eslint from '@eslint/js';
-import tseslint from '@typescript-eslint';
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
